d0ef8787cc9b238ce65f6c2194113b9e91e2bc0f
Ran Aharoni <ranaharoni@yahoo.com>
2022-04-11 18:21:46 +0200

    Initial commit

---
+++ src/ui/layouts/GenericLayout.tsx
@@ -0,0 +1,37 @@
+import React, {ReactNode} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export interface GenericLayoutProps {
+  children: ReactNode;
+}
+
+export const GenericLayout = ({children}: GenericLayoutProps) => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      {children}
+    </SafeAreaView>
+  );
+};
